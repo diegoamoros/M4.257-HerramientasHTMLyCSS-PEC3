7da0fa432c9682c25e92ce07fb57210c1be0f0f2
Diego Amoros <damoros@outlook.com>
2023-06-23 19:25:57 +0200

    commit

--- src/app.js
+++ src/app.js
@@ -1,2 +1,17 @@
-const name2 = 'world22222222222';
-console.log(`Hello ${name2}`);
+let imgObj1 = null;  
+function init() {  
+   imgObj1 = document.getElementById('header-logo-link');  
+   imgObj1.style.position= 'relative';   
+   imgObj1.style.left = '0px';   
+
+}  
+function rotate3() {  
+   imgObj1.style.transform = "rotate(3deg)";  
+}  
+
+function rotate0() {  
+  imgObj1.style.transform = "rotate(0deg)";  
+}  
+
+
+window.onload = init; 
